d1f76c532171fdeeedcaa0968313b6c23d81992b
peng <hong.zhang@isap.com.cn>
2017-06-12 19:14:11 +0800

    Up

---
+++ Conver To Sbo Icons/MainForm.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Conver_To_Sbo_Icons
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel4 = new MetroFramework.Controls.MetroLabel();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Linen;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(23, 151);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(16, 16);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.pictureBox1_DragDrop);
+            this.pictureBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.pictureBox1_DragEnter);
+            this.pictureBox1.QueryContinueDrag += new System.Windows.Forms.QueryContinueDragEventHandler(this.pictureBox1_QueryContinueDrag);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Linen;
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(56, 151);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(26, 18);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.DragDrop += new System.Windows.Forms.DragEventHandler(this.pictureBox1_DragDrop);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.FontWeight = MetroFramework.MetroLabelWeight.Regular;
+            this.metroLabel1.ForeColor = System.Drawing.Color.AliceBlue;
+            this.metroLabel1.Location = new System.Drawing.Point(23, 117);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(158, 19);
+            this.metroLabel1.TabIndex = 2;
+            this.metroLabel1.Text = "Drop  Image in the from";
+            this.metroLabel1.UseStyleColors = true;
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.FontWeight = MetroFramework.MetroLabelWeight.Regular;
+            this.metroLabel2.ForeColor = System.Drawing.Color.LavenderBlush;
+            this.metroLabel2.Location = new System.Drawing.Point(23, 60);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(382, 57);
+            this.metroLabel2.Style = MetroFramework.MetroColorStyle.Silver;
+            this.metroLabel2.TabIndex = 2;
+            this.metroLabel2.Text = "Image size should be 16-by-16 pixels for the menu bar, \r\nand 26-by-18 pixels for " +
+    "the main menu.\r\nspecific background color for transparent  R:192 G:220 B:192";
+            this.metroLabel2.UseStyleColors = true;
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.Location = new System.Drawing.Point(11, 170);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(39, 19);
+            this.metroLabel3.TabIndex = 4;
+            this.metroLabel3.Text = "16x16";
+            // 
+            // metroLabel4
+            // 
+            this.metroLabel4.AutoSize = true;
+            this.metroLabel4.Location = new System.Drawing.Point(56, 170);
+            this.metroLabel4.Name = "metroLabel4";
+            this.metroLabel4.Size = new System.Drawing.Size(41, 19);
+            this.metroLabel4.TabIndex = 5;
+            this.metroLabel4.Text = "26x18";
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(122, 151);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(47, 35);
+            this.metroButton1.TabIndex = 6;
+            this.metroButton1.Text = "Save";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(225, 121);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(148, 68);
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AllowDrop = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.ClientSize = new System.Drawing.Size(428, 209);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.metroLabel3);
+            this.Controls.Add(this.metroLabel4);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Resizable = false;
+            this.ShadowType = MetroFramework.Forms.MetroFormShadowType.AeroShadow;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Conver To Sbo Menu Image";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.pictureBox1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.pictureBox1_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroLabel metroLabel4;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
---
+++ Conver To Sbo Icons/MainForm.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Conver_To_Sbo_Icons
+{
+    using System.Drawing.Imaging;
+    using System.IO;
+
+    using ImageProcessor;
+    using ImageProcessor.Imaging;
+    using ImageProcessor.Imaging.Formats;
+
+    /// <summary>
+    /// The main form.
+    /// </summary>
+    public partial class MainForm : MetroFramework.Forms.MetroForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.pictureBox1.AllowDrop = true;
+            this.pictureBox2.AllowDrop = true;
+        }
+
+        string fileName;
+        private void pictureBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            fileName = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+            Image image;
+            try
+            {
+                 image = Image.FromFile(fileName);
+            }
+            catch
+            {
+                fileName = null;
+                this.pictureBox1.Image = null;
+                this.pictureBox2.Image = null;
+                return;
+            }
+
+            this.demo1(image);
+            //            this.pictureBox1.Image = image;
+            this.pictureBox1.SizeMode= PictureBoxSizeMode.Zoom;
+            this.pictureBox1.BackgroundImage = null;
+//            this.pictureBox2.Image = image  ;
+            this.pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            this.pictureBox2.BackgroundImage = null;
+        }
+
+        private void pictureBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Link;
+            else e.Effect = DragDropEffects.None;
+        }
+
+        public void demo1( Image inStream)
+        {
+//            byte[] photoBytes = File.ReadAllBytes(file);
+            // Format is automatically detected though can be changed.
+//            ISupportedImageFormat format = new JpegFormat { Quality = 70 };
+            Size size = new Size(16, 16);
+//            using (MemoryStream inStream = new MemoryStream(photoBytes))
+//            {
+                using (MemoryStream outStream = new MemoryStream())
+                {
+                    using (MemoryStream outStream2 = new MemoryStream())
+                    {
+                        // Initialize the ImageFactory using the overload to preserve EXIF metadata.
+                        using (ImageFactory imageFactory = new ImageFactory(preserveExifData: true))
+                        {
+                            // Load, resize, set the format and quality and save an image.
+                            imageFactory
+//                                .Pixelate(24)
+                                .Load(inStream)
+                                //                            .Resize(size)
+                                .Constrain(size)
+                                //                            .Format(format)
+                                .BackgroundColor(Color.FromArgb(192, 220, 192))
+                                .ReplaceColor(Color.Transparent, Color.FromArgb(192, 220, 192))
+                                .Save(outStream)
+                                .Resize(new ResizeLayer(new Size(26, 18), ResizeMode.BoxPad, AnchorPosition.Left))
+                                .BackgroundColor(Color.FromArgb(192, 220, 192))
+                                .Save(outStream2)
+                                ;
+                    }
+                        // Do something with the stream.
+                        pictureBox1.Image = Image.FromStream(outStream);
+                        pictureBox2.Image = Image.FromStream(outStream2);
+
+                    //                }
+                }
+                }
+        }
+
+        private void pictureBox1_QueryContinueDrag(object sender, QueryContinueDragEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            var img = pictureBox1.Image;
+            if (img == null) return;
+            if (DoDragDrop(img, DragDropEffects.Move) == DragDropEffects.Move)
+            {
+            
+            }
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            if (fileName!=null)
+            {
+//                int position = fileName.LastIndexOf(".");
+//                string fileName = fileName.Substring(0, position - 1);
+////                活学活用：
+//                System.IO.Path.GetFileName(fileName); //返回带扩展名的文件名  
+                var fileNameWithoutExtension = System.IO.Path.GetFileNameWithoutExtension(this.fileName);
+                var directoryName = System.IO.Path.GetDirectoryName(this.fileName);
+                var path16 = Path.Combine(directoryName, fileNameWithoutExtension + "_16x16.bmp");
+                var path26 = Path.Combine(directoryName, fileNameWithoutExtension + "_26x18.bmp");
+                if (File.Exists(path16))
+                {
+                    File.Delete(path16);
+                }
+                if (File.Exists(path26))
+                {
+                    File.Delete(path26);
+                }
+                //                using (ImageFactory imageFactory = new ImageFactory(preserveExifData: true))
+                //                {
+                //                    imageFactory.Load(pictureBox1.Image).Save(path16);
+                //                }
+                //                using (ImageFactory imageFactory = new ImageFactory(preserveExifData: true))
+                //                {
+                //                    imageFactory.Load(pictureBox2.Image).Save(path26);
+                //                }
+                save24bt(pictureBox1.Image, path16);
+                save24bt(pictureBox2.Image, path26);
+                //                                pictureBox1.Image.Save(path16, ImageFormat.Bmp);
+                //                                pictureBox2.Image.Save(path26, ImageFormat.Bmp);
+                OpenFolderAndSelectFile(path16);
+
+                //                fileName = null;
+                //                this.pictureBox1.Image = null;
+                //                this.pictureBox2.Image = null;
+            }
+        }
+
+        public void save24bt(Image imgCheque,string filepath)
+        {
+            using (Bitmap blankImage = new Bitmap(imgCheque.Width, imgCheque.Height, PixelFormat.Format24bppRgb))
+            {
+                using (Graphics g = Graphics.FromImage(blankImage))
+                {
+                    g.DrawImageUnscaledAndClipped(imgCheque, new Rectangle(Point.Empty, imgCheque.Size));
+                }
+                blankImage.Save(filepath, ImageFormat.Bmp);
+            }
+        }
+
+        /// <summary>
+        /// 在资源管理器中打开文件夹并定位文件
+        /// </summary>
+        /// <param name="fileFullName"></param>
+        private void OpenFolderAndSelectFile(String fileFullName)
+        {
+            System.Diagnostics.ProcessStartInfo psi = new System.Diagnostics.ProcessStartInfo("Explorer.exe");
+            psi.Arguments = "/e,/select," + fileFullName;
+            System.Diagnostics.Process.Start(psi);
+        }
+    }
+}
