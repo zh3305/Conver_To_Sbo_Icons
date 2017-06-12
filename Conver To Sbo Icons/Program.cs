61f7c7fe16a82e4e5b048cefc3b782557f9afd38
peng <hong.zhang@isap.com.cn>
2017-06-12 19:14:11 +0800

    Up

--- Conver To Sbo Icons/Program.cs
+++ Conver To Sbo Icons/Program.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Conver_To_Sbo_Icons
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
+            System.Globalization.CultureInfo cultureInfo = new System.Globalization.CultureInfo("de-DE");
+            Application.CurrentCulture = cultureInfo;
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
         }
     }
 }
